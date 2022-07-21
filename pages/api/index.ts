6c3cb831b9fc3f3a857915cf658e6a5367ce8124
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-07-21 09:56:07 -0300

    removed api/ endpoint

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,9 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { firestore } from "lib/firestore";
 import { Auth } from "models/auth";
-import { findOrCreateAuth, sendCode } from "controllers/auth";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const auth = await sendCode(req.body.email);
-  res.send(auth);
-}
+
