907559b3189046d5eed45b178f682c9f5c1377d8
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-07-28 15:46:30 -0300

    removed birthday user information

--- controllers/user.ts
+++ controllers/user.ts
@@ -18,16 +18,6 @@ export async function updateUserData(token, updateData) {
   const user = new User(token.userId);
   await user.get();
 
-  // si el usuario provee fecha de nacimiento
-  if (updateData.birthday) {
-    console.log(updateData.birthday);
-    
-    const birthdayFromUpdateData = new Date (updateData.birthday.year, updateData.birthday.month -1, updateData.birthday.day, 0, 0)
-    console.log(birthdayFromUpdateData);
-    
-    updateData.birthday = birthdayFromUpdateData;
-  }
-
   user.data = {
     ...updateData,
   };
